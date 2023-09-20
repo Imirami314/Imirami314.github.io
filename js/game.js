968f18c91da0bc2a71656c8f7ee94c935e06e1a0
puzzleCatcher <74927928+puzzleCatcher@users.noreply.github.com>
2023-09-19 20:39:17 -0700

    removed test grass path
    rip chung

--- js/game.js
+++ js/game.js
@@ -2121,14 +2121,12 @@ if (!!save) {
 // ONLY TURN THIS ON USING CONSOLE
 // TO TURN DEV OFF, RELOAD
 
-var chung = finder.findPath(153, 45, 185, 48, mainMap.grid)
+// var chung = finder.findPath(153, 45, 185, 48, mainMap.grid)
 
-for (var i in chung) {
-  var ch = chung[i]
-  mainMap.changeBlock(ch[0], ch[1], ",")
-}
-
-console.log(chung)
+// for (var i in chung) {
+//   var ch = chung[i]
+//   mainMap.changeBlock(ch[0], ch[1], ",")
+// }
 
 var gameInterval = setInterval(function() {
   if (tabIsActive) {
